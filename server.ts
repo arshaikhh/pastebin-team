8b01bf5caa20ae19f65cf2ae133088de4687fc7c
Abdul Rehman Shaikh <CORP\2202-001-ars@a-hw0jpshz94r.corp.amazonworkspaces.com>
2022-04-25 20:31:55 +0500

    modified creationDate

--- server.ts
+++ server.ts
@@ -37,7 +37,8 @@ app.get("/", async (req, res) => {
 app.post("/", async (req,res)=> {
   try {
     const input = req.body
-    const dbres = await client.query('insert into pasteData(title,name,creation_date) values($1,$2,$3) returning *',[input.title,input.data,input.creation_date])
+    const dbres = await client.query('insert into pasteData(title,name,creation_date) values($1,$2,$3) returning *',[input.title,input.data,input.creationDate])
+    console.log(input.data)
     res.json(dbres.rows[0])
   }
   catch(err) {
